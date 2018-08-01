7a28549f0f5c20c6e483b29d227a251e1fa279e4
patbeagan1 <pbeagan1@gmail.com>
2018-07-31 22:59:56 -0400

    Adding in some new scripts for comicification and gallery viewing

---
+++ comicify.sh
@@ -0,0 +1,32 @@
+f ()
+{
+    comic ()
+    {
+        name=$(echo "$1" | sed 's/\///g')
+        zip -r "$name".zip "$name";
+        mv "$name".zip "$name".cbz
+    }
+
+    for i in $(ls | grep -v .cbz);
+    do
+        match='false';
+        for j in $(ls | grep .cbz);
+        do
+            if [[ "$j" =~ "$i" ]]; then
+                match='true';
+                break;
+            fi;
+        done;
+        if [ $match = 'true' ]; then
+            echo 'comicified   '"$i";
+        else
+            [[ -d NOZIP ]] || mkdir NOZIP
+            if [ "$i" != "NOZIP" ]; then
+                echo 'comic create '"$i";
+                mv "$i"/*.webm NOZIP
+                comic "$i";
+            fi
+        fi;
+    done
+}
+f
