672700917555574d8f449ea304247321c3fffdc6
Patrick Beagan <pbeagan1@gmail.com>
2020-05-02 03:06:22 -0400

    Fixing source files to no longer be executable

--- source-settings-libraries.sh
+++ source-settings-libraries.sh
@@ -38,3 +38,4 @@ fill_img_cache () { cache_get "$IMG_CACHE"/"$1" "$2"; }
 
 fill_img_cache scenic.jpg "http://images.hdrsoft.com/images/contest/realistic_prize1_spring2015.jpg"
 fill_img_cache parallel.jpg "https://i.imgur.com/mmdO5bx.png"
+fill_img_cache transparency.png "http://www.netpulse.com/overview-src/img/connect-icon-transparent.png"
