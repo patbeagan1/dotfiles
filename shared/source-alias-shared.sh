4079094fbac73f512c8721d8e0313766f0adee4e
Patrick Beagan <pbeagan1@gmail.com>
2020-10-28 21:52:56 -0400

    adding in java version utils

--- shared/source-alias-shared.sh
+++ shared/source-alias-shared.sh
@@ -17,7 +17,14 @@ alias dict="curl dict://dict.org/d:"
 alias define="curl dict://dict.org/d:"
 alias lynx='lynx -accept_all_cookies'
 
-#####################
+############
+### Java ###
+############
+alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias j7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`; java -version"
+
+####################
 ### Image Editing ###
 #####################
 
