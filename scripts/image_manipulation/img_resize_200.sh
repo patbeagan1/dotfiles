3b870390b2082d22da5611316499fafca64952e2
Patrick Beagan <pbeagan1@gmail.com>
2021-07-14 20:00:27 -0500

    updates

---
+++ scripts/image_manipulation/img_resize_200.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+img_resize ()
+{
+    in="$1";
+    out="$2";
+    convert "$in" -resize 200x200 -gravity center -extent 200x200 -fill black "$out"
+}
+
+img_resize "$@"
---
+++ scripts/util/ipqr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qrencode -l L -v 1 -o /tmp/qr-out.png -r <(echo http://"$(ipconfig getifaddr en0)":8000) && open /tmp/qr-out.png
