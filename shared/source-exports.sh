01fcb9f9faa7ecd6c8c237b6d7cca78a79fa38db
Patrick Beagan <pbeagan@wayfair.com>
2021-08-30 16:09:36 -0500

    updates

--- shared/source-exports.sh
+++ shared/source-exports.sh
@@ -1,4 +1,13 @@
 
+export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+
+alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java11='export JAVA_HOME=$JAVA_11_HOME'
+
+# default to Java 11
+java11
+
 export ANDROID_SDK=$HOME/Library/Android/sdk
 export PATH=$ANDROID_SDK/emulator:$ANDROID_SDK/tools:$PATH
 export PATH=$ANDROID_SDK/platform-tools:$PATH
