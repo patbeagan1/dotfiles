5f3249624ee9288ad0666d41d09a2e131d524676
Patrick Beagan <pbeagan1@gmail.com>
2021-09-26 15:21:12 -0500

    removing shaders

--- glsl/rainbowstar.glsl
+++
@@ -1,9 +0,0 @@
-void mainImage(out vec4 f,vec2 q){
-
-    vec2 r = iResolution.xy, 
-         p = (q-.5*r)/r.y;
-
-    float l=length(p),z=iTime;
-	for( int i=0; i<4; i++)
-		f[i] = .01/length(abs(fract( q/r + p/l* (sin(z+=.07)+1.) * abs(sin(l*9.-z*2.)) )-.5)) / l;
-}
