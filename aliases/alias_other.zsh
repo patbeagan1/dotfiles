744f9045b6554f604ba5d503f33876b631c02c2d
Patrick Beagan <pbeagan@wayfair.com>
2022-02-08 09:42:38 -0600

    using a better filename for the tp command

--- aliases/alias_other.zsh
+++ aliases/alias_other.zsh
@@ -1,7 +1,7 @@
 alias readOutLoud='say -v Moira -i -f'
 alias read_out_loud=readOutLoud
 
-alias waypoint='echo `pwd` >> ~/waypoint.txt; cat ~/waypoint.txt | sort | uniq >> ~/waypoint2.txt; mv ~/waypoint2.txt ~/waypoint.txt'
+alias waypoint='echo `pwd` >> ~/waypoint.txt; cat ~/waypoint.txt | sort | uniq >> /tmp/waypoint.txt; mv /tmp/waypoint.txt ~/waypoint.txt'
 waypoint_go () { cd $(cat ~/waypoint.txt | fzf -1 -q "$1") ; }
 alias teleport=waypoint_go
 alias tp=teleport
