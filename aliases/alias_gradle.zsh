a9d4b315125ff5aa2019150424119ddab2630157
Patrick Beagan <pbeagan@wayfair.com>
2022-02-08 14:20:48 -0600

    adding in a destroy gradle script

--- aliases/alias_gradle.zsh
+++ aliases/alias_gradle.zsh
@@ -1,3 +1,4 @@
+alias destroy_gradle='rm -rf ~/.gradle/caches && rm -rf .gradle && ./gradlew clean'
 alias findgradle='\ps aux | grep Gradle | grep -v grep | awk '\''{print $2}'\'''
 alias killgradle='findgradle | xargs kill -9'
 alias gradlekill='pkill -f gradle-launcher'
