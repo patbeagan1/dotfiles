667c428c9e8ff71822e930cfbb37cb4a35f280e2
patbeagan1 <pbeagan1@gmail.com>
2022-05-08 23:31:47 -0500

    adding in the note and battery scripts

--- aliases/alias_network.zsh
+++ aliases/alias_network.zsh
@@ -1,3 +1,4 @@
+alias dns_check='systemd-resolve --status'
 alias ports='netstat -vanp tcp'
 alias ip_local="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
