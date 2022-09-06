188fd4a50b625d2af29237e23ebe6de98f5f602a
Patrick Beagan <pbeagan1@gmail.com>
2022-09-06 18:33:00 -0500

    Adding in the scriptify alias

---
+++ aliases/alias_scriptify.zsh
@@ -0,0 +1,40 @@
+scriptify() {
+    create_header() {
+        cat <<EOF
+
+set -euo pipefail
+IFS=$'\n\t'
+
+help () {
+    error_code=\$?
+    echo "
+No help message yet
+"
+    if [[ ! \$error_code -eq 0 ]]; then echo "Err: \$error_code"; fi
+}
+
+EOF
+    }
+# ==============================================================
+
+    if [[ "$(which "$1")" == *": shell reserved word" ]]; then
+        echo This is a shell builtin.
+    elif [[ "$(which "$1")" == *": aliased to"* ]]; then
+        echo This is an alias.
+    else
+# ==============================================================
+
+        # Printing out the contents of the function to a file and making it executable
+        echo '#!/usr/bin/env zsh' >"$1".sh &&
+            if test $?; then
+                chmod 755 "$1".sh
+                which "$1" | cat
+                create_header >>"$1".sh
+                which "$1" >>"$1".sh
+                echo >>"$1".sh
+                echo "$1" '"$@" || help' >>"$1".sh
+                echo 'trackusage.sh "$0"' >>"$1".sh
+            fi
+
+    fi
+}
