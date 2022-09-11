3fab15f64398b7fb649ebc05355da5a08b785ffe
Patrick Beagan <pbeagan1@gmail.com>
2022-09-10 20:20:52 -0500

    changing to deno fmt

--- scripts/util/mtg-booster.js
+++ scripts/util/mtg-booster.js
@@ -1,30 +1,32 @@
 #!/usr/bin/env deno run --allow-net --allow-write
 
-const startUrl = 'https://api.scryfall.com/cards/search' +
-  '?format=json' +
-  '&include_extras=true' +
-  '&include_multilingual=false' +
-  '&include_variations=true' +
-  '&order=set' +
-  '&page=1' +
-  '&q=e%3Admu' +
-  '&unique=prints'
+const startUrl = "https://api.scryfall.com/cards/search" +
+  "?format=json" +
+  "&include_extras=true" +
+  "&include_multilingual=false" +
+  "&include_variations=true" +
+  "&order=set" +
+  "&page=1" +
+  "&q=e%3Admu" +
+  "&unique=prints";
 
-const groupCardsByRarity = (cards) => cards
-  .filter((it) => !it.type_line.includes('Basic Land'))
-  .reduce((acc, value) => {
-    if (!acc[value.rarity]) {
-      acc[value.rarity] = []
-    }
-    acc[value.rarity].push(value)
-    return acc
-  }, {})
+const groupCardsByRarity = (cards) =>
+  cards
+    .filter((it) => !it.type_line.includes("Basic Land"))
+    .reduce((acc, value) => {
+      if (!acc[value.rarity]) {
+        acc[value.rarity] = [];
+      }
+      acc[value.rarity].push(value);
+      return acc;
+    }, {});
 
-const findBasicLands = (cards) => cards
-  .filter((it) => it.type_line.includes('Basic Land'))
+const findBasicLands = (cards) =>
+  cards
+    .filter((it) => it.type_line.includes("Basic Land"));
 
 async function getNextPage(url) {
-  console.log('Visit next page')
+  console.log("Visit next page");
   return fetch(url)
     .then((response) => response.json())
     .then(async (response) => {
@@ -35,45 +37,53 @@ async function getNextPage(url) {
           rarity: it.rarity,
           scryfall_uri: it.scryfall_uri,
           image_url: it.image_uris.small,
-          type_line: it.type_line
-        }
-      })
+          type_line: it.type_line,
+        };
+      });
       if (response.has_more) {
-        out.push.apply(out, await getNextPage(response.next_page))
+        out.push.apply(out, await getNextPage(response.next_page));
       }
-      return out
+      return out;
     })
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 }
 
 function getRandomByRarity(cardsByRarity, rarity) {
-  return cardsByRarity[rarity][Math.floor(Math.random() * cardsByRarity[rarity].length)]
+  return cardsByRarity[rarity][
+    Math.floor(Math.random() * cardsByRarity[rarity].length)
+  ];
 }
 
 function getRandomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const a = getNextPage(startUrl)
+const a = getNextPage(startUrl);
 async function main() {
-  const cardsByRarity = groupCardsByRarity(await a)
-  const basicLands = findBasicLands(await a)
+  const cardsByRarity = groupCardsByRarity(await a);
+  const basicLands = findBasicLands(await a);
   const cards = [
-    Array(10).fill(0).map(() => getRandomByRarity(cardsByRarity, 'common')),
-    Array(3).fill(0).map(() => getRandomByRarity(cardsByRarity, 'uncommon')),
-    [Math.floor(Math.random() * 8) === 0
-      ? getRandomByRarity(cardsByRarity, 'mythic')
-      : getRandomByRarity(cardsByRarity, 'rare')],
-    [getRandomFromArray(basicLands)]
-  ].flatMap((it) => it)
+    Array(10).fill(0).map(() => getRandomByRarity(cardsByRarity, "common")),
+    Array(3).fill(0).map(() => getRandomByRarity(cardsByRarity, "uncommon")),
+    [
+      Math.floor(Math.random() * 8) === 0
+        ? getRandomByRarity(cardsByRarity, "mythic")
+        : getRandomByRarity(cardsByRarity, "rare"),
+    ],
+    [getRandomFromArray(basicLands)],
+  ].flatMap((it) => it);
 
-  console.log(cards)
+  console.log(cards);
 
   const output = `
   <html><body>
-  ${cards.map((it) => `<a href="${it.scryfall_uri}"><img src="${it.image_url}"/></a>`).join('\n')}
+  ${
+    cards.map((it) =>
+      `<a href="${it.scryfall_uri}"><img src="${it.image_url}"/></a>`
+    ).join("\n")
+  }
   </body></html>
-`
-  await Deno.writeTextFile('./mtg-booster.html', output)
+`;
+  await Deno.writeTextFile("./mtg-booster.html", output);
 }
-main()
+main();
