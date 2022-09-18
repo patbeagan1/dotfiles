711c6fc88a2c77c68fbddf5a4177e5a74207c172
Patrick Beagan <pbeagan1@gmail.com>
2022-09-18 13:04:14 -0500

    formatting

--- scripts/util/simplify-prose.sh
+++ scripts/util/simplify-prose.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 help() {
-    error_code=$?
-    echo "
+  error_code=$?
+  echo "
 Helps to break up large blocks of text by putting common punctuation on different lines.
   Paragraphs are shown with horizontal rules,
   Sentences are shown by new paragraphs,
@@ -23,64 +23,64 @@ Errs:
   1: generic
   2: missing mode flag
 "
-    if [[ ! $error_code -eq 0 ]]; then echo "Err: $error_code"; fi
+  if [[ ! $error_code -eq 0 ]]; then echo "Err: $error_code"; fi
 }
 
 remove_leading_whitespace() {
-    # removes leading whitespace
-    sed 's/^[ ]*\(.*$\)/\1/' #|
+  # removes leading whitespace
+  sed 's/^[ ]*\(.*$\)/\1/' #|
 }
 
-replace_abbreviations () {
-        sed 's/i\.e\./i-e-/g' |
-        sed 's/_i\.e_\./i-e-/g' |
-        sed 's/e\.g\./e-g-/g' 
+replace_abbreviations() {
+  sed 's/i\.e\./i-e-/g' |
+    sed 's/_i\.e_\./i-e-/g' |
+    sed 's/e\.g\./e-g-/g'
 }
 
-replace_quoted_punctuation () {
-          sed 's/\."/"\./g' |
-        sed 's/\.”/”\./g' |
-        sed 's/\,"/"\,/g' |
-        sed 's/\,”/”\,/g' 
+replace_quoted_punctuation() {
+  sed 's/\."/"\./g' |
+    sed 's/\.”/”\./g' |
+    sed 's/\,"/"\,/g' |
+    sed 's/\,”/”\,/g'
 }
 
-replace_punctuation () {
-        sed 's/,/,\n /g' |
-        sed 's/;/;\n /g' |
-        sed 's/\./\.\n\n/g' |
-        sed 's/\?/\?\n\n/g' |
-        sed 's/\!/\!\n\n/g' 
+replace_punctuation() {
+  sed 's/,/,\n /g' |
+    sed 's/;/;\n /g' |
+    sed 's/\./\.\n\n/g' |
+    sed 's/\?/\?\n\n/g' |
+    sed 's/\!/\!\n\n/g'
 }
 
-replace_double_spaces () {
-  sed 's/  / /g' 
+replace_double_spaces() {
+  sed 's/  / /g'
 }
 
 simplify-prose-legal() {
-    cat "$1" |
-        remove_leading_whitespace |
-        tr "\n" " " |
-        replace_double_spaces |
-        replace_abbreviations |
-        replace_quoted_punctuation |
-        replace_punctuation |
-        awk '{print ++count "\t" "| " $0}'
+  cat "$1" |
+    remove_leading_whitespace |
+    tr "\n" " " |
+    replace_double_spaces |
+    replace_abbreviations |
+    replace_quoted_punctuation |
+    replace_punctuation |
+    awk '{print ++count "\t" "| " $0}'
 }
 
 simplify-prose() {
-    cat "$1" |
-        remove_leading_whitespace |
-        tr '\n' '\r' |
-        sed 's/\r\r/NEWLINE-MAGIC-9182/g' |
-        sed 's/NEWLINE-MAGIC-9182NEWLINE-MAGIC-9182/\n\n/g' |
-        sed 's/NEWLINE-MAGIC-9182/ /g' |
-        tr "\n" "\r" |
-        sed 's/\r\r/\n--------\n\n /g' |
-        replace_double_spaces |
-        replace_abbreviations |
-        replace_quoted_punctuation |
-        replace_punctuation |
-        awk '/--------/ {print ++count, $0} !/--------/ {print}'
+  cat "$1" |
+    remove_leading_whitespace |
+    tr '\n' '\r' |
+    sed 's/\r\r/NEWLINE-MAGIC-9182/g' |
+    sed 's/NEWLINE-MAGIC-9182NEWLINE-MAGIC-9182/\n\n/g' |
+    sed 's/NEWLINE-MAGIC-9182/ /g' |
+    tr "\n" "\r" |
+    sed 's/\r\r/\n--------\n\n /g' |
+    replace_double_spaces |
+    replace_abbreviations |
+    replace_quoted_punctuation |
+    replace_punctuation |
+    awk '/--------/ {print ++count, $0} !/--------/ {print}'
 }
 
 if [ "$1" = "-l" ]; then
