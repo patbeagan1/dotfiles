c6070165f92947ee4b33cc207a8cdedcdc34a30c
Patrick Beagan <pbeagan1@gmail.com>
2022-09-22 22:52:44 -0500

    fixing up the japanese query

--- aliases/alias_network.zsh
+++ aliases/alias_network.zsh
@@ -5,7 +5,10 @@ alias ip_local="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 alias ps-port='lsof -nP -iTCP -sTCP:LISTEN | grep'
 alias psport='netstat -vanp tcp | grep 127.0.0.1'
-# translate-japanese () { open -a Safari 'https://translate.google.com/?sl=en&tl=ja&op=translate&text='"$1"; }
+translate-japanese() {
+    local query="$(echo "$1" | sed 's/ /%20/g')"
+    open -a Safari 'https://translate.google.com/?sl=en&tl=ja&op=translate&text='"$query"
+}
 share() {
     ssh -R 80:localhost:$1 nokey@localhost.run
 }
