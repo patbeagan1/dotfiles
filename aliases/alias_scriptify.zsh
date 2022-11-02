02d1ea44470f9334f80013979a0c16fe739d10b8
patbeagan1 <pbeagan1@gmail.com>
2022-11-01 22:01:24 -0500

    Improving scriptify - it can now generate code that handles arguments

--- aliases/alias_scriptify.zsh
+++ aliases/alias_scriptify.zsh
@@ -1,28 +1,42 @@
 scriptify() {
     create_header() {
-        cat <<EOF
-
+        echo '
 set -euo pipefail
-IFS=$'\n\t'
 
-help () {
-    error_code=\$?
+scriptname="$0"
+help() {
+    error_code=$?
     echo "
+Usage: $scriptname [-h|--help]
+
 No help message yet
 "
-    exit \$error_code
+    exit $error_code
 }
-
-EOF
+'
     }
-# ==============================================================
+    create_parser() {
+        echo '
+    emulate -L zsh
+    zmodload zsh/zutil || return 1
+
+    local help
+    zparseopts -D -F -K -- \
+        {h,-help}=help ||
+        return 1
+
+    if (($#help)); then help; fi
+'
+    }
+
+    # ==============================================================
 
     if [[ "$(which "$1")" == *": shell reserved word" ]]; then
         echo This is a shell builtin.
     elif [[ "$(which "$1")" == *": aliased to"* ]]; then
         echo This is an alias.
     else
-# ==============================================================
+        # ==============================================================
 
         # Printing out the contents of the function to a file and making it executable
         echo '#!/usr/bin/env zsh' >"$1".sh &&
@@ -30,9 +44,13 @@ EOF
                 chmod 755 "$1".sh
                 which "$1" | cat
                 create_header >>"$1".sh
+                echo "main() {" >>"$1".sh
+                create_parser >>"$1".sh
                 which "$1" >>"$1".sh
                 echo >>"$1".sh
-                echo "$1" '"$@" || help' >>"$1".sh
+                echo "    $1" '"$@" || help' >>"$1".sh
+                echo "}\n" >>"$1".sh
+                echo 'main "$@" || help' >>"$1".sh
                 echo 'trackusage.sh "$0"' >>"$1".sh
             fi
 
