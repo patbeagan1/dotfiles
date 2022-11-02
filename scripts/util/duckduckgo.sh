b91fbf730f2dd21ab97b92b1a705d54c26e75108
patbeagan1 <pbeagan1@gmail.com>
2022-11-01 23:02:55 -0500

    adding poor quality sites to the ddg filter

--- scripts/util/duckduckgo.sh
+++ scripts/util/duckduckgo.sh
@@ -14,11 +14,18 @@ No help message yet
 q() {
     local content='https://duckduckgo.com/'
     
+    content+=$(echo "$@" | sed 's/ /%20/g')
+    content+='%20'
+
     # blocklist
     content+='-site%3Aquora.com%20'
     content+='-site%3Ayummly.com%20'
+    content+='-site%3Aamazon.com%20'
+    content+='-site%3Atripadvisor.com%20'
+    content+='-site%3Aexpedia.com%20'
+    content+='-site%3Afacebook.com%20'
+    content+='-site%3Amicrosoft.com%20'
 
-    content+=$(echo "$@" | sed 's/ /%20/g')
 
     if isMac.sh; then
         open -a Safari "$content"
