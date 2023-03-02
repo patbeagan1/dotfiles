fb7f353bb2f8f54d7e5f8cf80d119f0102196321
pbeagan <pbeagan1@gmail.com>
2023-03-02 12:29:29 -0600

    updating with new framework computer

--- configs/config-zsh.zsh
+++ configs/config-zsh.zsh
@@ -13,6 +13,31 @@ setopt NO_AUTO_MENU
 setopt NO_CASE_GLOB
 setopt NO_MENU_COMPLETE
 setopt SHARE_HISTORY
+setopt histignorealldups sharehistory
 
-# Fixes autocomplete to choose the case sensitive one first, if it exists.
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# Use emacs keybindings even if our EDITOR is set to vi
+bindkey -e
+
+# Use modern completion system
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+
