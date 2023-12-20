0627982522cc2e8f17a2d89f3feb082714850f4e
pbeagan <pbeagan1@gmail.com>
2023-12-20 14:03:23 -0600

    adding in the border

--- scripts/util/xmas.py
+++ scripts/util/xmas.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-import random
 import argparse
+import random
 
-def create_christmas_tree(height, tree_color, bauble_color, garland_color):
-    """Generate a Christmas tree ASCII art with baubles and garlands."""
+current_line = 1
+def line_print(s):
+    global current_line
+    print(f"{str(current_line).zfill(2)} {s}")
+    current_line += 1
+
+def create_christmas_tree(height, tree_color, bauble_color, garland_color, border_color):
+    """Generate a Christmas tree ASCII art with baubles, garlands, and a festive border."""
+    border = f"   {border_color}{'*' * (height * 2 + 1)}\033[0m"
+    print(border)
     for i in range(height):
         row = ''
         for j in range(2 * i + 1):
             char = '*' if random.random() > 0.1 else 'O'
             color = bauble_color if char == 'O' else garland_color if j % 3 == 0 else tree_color
             row += f"{color}{char}\033[0m"
-        print(f"{' ' * (height - i - 1)}{row}")
-    print(f"{tree_color}{(' ' * (height - 1)) + '*'}\033[0m")
+        line_print(f"{border_color}*{tree_color}{' ' * (height - i - 1)}{row}{border_color}{' ' * (height - i - 1)}*\033[0m")
+    print(f"   {border_color}*{tree_color}{(' ' * (height - 1)) + '*'}{border_color}{' ' * (height - 1)}*\033[0m")
+    print(border)
 
 def convert_color(color_str):
     """Converts a color string 'R,G,B' to an ANSI escape sequence."""
@@ -20,11 +29,12 @@ def convert_color(color_str):
 
 def parse_arguments():
     """Parse command line arguments for the script."""
-    parser = argparse.ArgumentParser(description='Generate a festive Christmas tree ASCII art.')
+    parser = argparse.ArgumentParser(description='Generate a festive Christmas tree ASCII art with a border.')
     parser.add_argument('-H', '--height', type=int, default=10, help='Height of the Christmas tree')
     parser.add_argument('-C', '--color', type=str, default='34,139,34', help='RGB color for the tree')
     parser.add_argument('-B', '--bauble', type=str, default='255,0,0', help='RGB color for the baubles')
     parser.add_argument('-G', '--garland', type=str, default='255,215,0', help='RGB color for the garlands')
+    parser.add_argument('-BR', '--border', type=str, default='128,128,128', help='RGB color for the border')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -32,5 +42,6 @@ if __name__ == "__main__":
     tree_color = convert_color(args.color)
     bauble_color = convert_color(args.bauble)
     garland_color = convert_color(args.garland)
-    create_christmas_tree(args.height, tree_color, bauble_color, garland_color)
+    border_color = convert_color(args.border)
+    create_christmas_tree(args.height, tree_color, bauble_color, garland_color, border_color)
 
