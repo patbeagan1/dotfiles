2a5cc5a9e43bad4103292baaa8555ce23b021d00
pbeagan <pbeagan1@gmail.com>
2023-12-20 12:29:18 -0600

    updating color work

--- scripts/util/xmas.py
+++ scripts/util/xmas.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
-import argparse
 import random
+import argparse
 
-def create_christmas_tree(height, color_code, bauble_code, garland_code):
+def create_christmas_tree(height, tree_color, bauble_color, garland_color):
     """Generate a Christmas tree ASCII art with baubles and garlands."""
     for i in range(height):
         row = ''
         for j in range(2 * i + 1):
             char = '*' if random.random() > 0.1 else 'O'
-            color = bauble_code if char == 'O' else garland_code if j % 3 == 0 else color_code
+            color = bauble_color if char == 'O' else garland_color if j % 3 == 0 else tree_color
             row += f"{color}{char}\033[0m"
         print(f"{' ' * (height - i - 1)}{row}")
-    print(f"{color_code}{(' ' * (height - 1)) + '*'}\033[0m")
+    print(f"{tree_color}{(' ' * (height - 1)) + '*'}\033[0m")
+
+def convert_color(color_str):
+    """Converts a color string 'R,G,B' to an ANSI escape sequence."""
+    r, g, b = map(int, color_str.split(','))
+    return f"\033[38;2;{r};{g};{b}m"
 
 def parse_arguments():
     """Parse command line arguments for the script."""
     parser = argparse.ArgumentParser(description='Generate a festive Christmas tree ASCII art.')
     parser.add_argument('-H', '--height', type=int, default=10, help='Height of the Christmas tree')
-    parser.add_argument('-C', '--color', type=str, default='\033[32m', help='ANSI color code for the tree')
-    parser.add_argument('-B', '--bauble', type=str, default='\033[31m', help='ANSI color code for the baubles')
-    parser.add_argument('-G', '--garland', type=str, default='\033[33m', help='ANSI color code for the garlands')
+    parser.add_argument('-C', '--color', type=str, default='34,139,34', help='RGB color for the tree')
+    parser.add_argument('-B', '--bauble', type=str, default='255,0,0', help='RGB color for the baubles')
+    parser.add_argument('-G', '--garland', type=str, default='255,215,0', help='RGB color for the garlands')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
-    create_christmas_tree(args.height, args.color, args.bauble, args.garland)
+    tree_color = convert_color(args.color)
+    bauble_color = convert_color(args.bauble)
+    garland_color = convert_color(args.garland)
+    create_christmas_tree(args.height, tree_color, bauble_color, garland_color)
 
